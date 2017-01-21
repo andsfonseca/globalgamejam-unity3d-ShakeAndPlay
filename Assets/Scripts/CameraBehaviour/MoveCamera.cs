136d211fc825d7481b3478e6dbd1a1985e7b1cf6
paulorcmendes <mendesdpn@gmail.com>
2017-01-21 11:44:31 -0300

    movimentação do player e camera

---
+++ Assets/Scripts/CameraBehaviour/MoveCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+	public Transform playerTransform;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3((playerTransform.position+offset).x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+	public float speed;
+	public float jumpForce;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (speed*Time.deltaTime, 0, 0);
+		if(Input.GetKeyDown("space")){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce*Time.deltaTime));
+		}
+	}
+}
