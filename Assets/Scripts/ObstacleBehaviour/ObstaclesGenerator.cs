b908a4a3c37d0c0e5ccfad29ce9ed24aec0dd4c6
andsfonseca <andsf@live.com>
2017-01-22 01:33:26 -0300

    correão de bugs na geraão de obstaculos

--- Assets/Scripts/ObstacleBehaviour/ObstaclesGenerator.cs
+++ Assets/Scripts/ObstacleBehaviour/ObstaclesGenerator.cs
@@ -7,24 +7,26 @@ public class ObstaclesGenerator : MonoBehaviour {
 	public GameObject boxPrefab;
 	public float timeRate;
 	private float lastSpawn;
-	private Vector3 offset;
+	//private Vector3 offset;
 	public Transform playerTransform;
-	// Use this for initialization
-	void Start () {
-		offset = transform.position - playerTransform.position;
+    private float speed;
+    // Use this for initialization
+    void Start () {
+		//offset = transform.position - playerTransform.position;
 		lastSpawn = 0;
-	}
+        speed = GameObject.FindGameObjectWithTag("Player").GetComponent<MovePlayer>().speed;
+    }
 
 	// Update is called once per frame
 	void Update () {
         if (GameLogic.Instance.gameStateManager.current.Equals(EGameState.GAME)) {
-            transform.position = new Vector3((playerTransform.position + offset).x, transform.position.y, transform.position.z);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
             if (Time.time - lastSpawn > timeRate) {
                 boxPrefab.Spawn(transform.position);
                 lastSpawn = Time.time;
             }
             foreach (GameObject box in GameObject.FindGameObjectsWithTag("box")) {
-                if ((transform.position - box.transform.position).x > 40) {
+                if ((playerTransform.position - box.transform.position).x > 40) {
                     box.Recycle();
                 }
             }
